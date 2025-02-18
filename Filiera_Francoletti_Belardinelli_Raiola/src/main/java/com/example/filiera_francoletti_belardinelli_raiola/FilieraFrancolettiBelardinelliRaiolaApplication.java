2966653b1f4d4998dfa98d9cac73adfd7ee6f93c
Frenco2003 <marco.francoletti@studenti.unicam.it>
2025-02-18 19:12:40 +0100

    create basic project

---
+++ Filiera_Francoletti_Belardinelli_Raiola/src/main/java/com/example/filiera_francoletti_belardinelli_raiola/FilieraFrancolettiBelardinelliRaiolaApplication.java
@@ -0,0 +1,13 @@
+package com.example.filiera_francoletti_belardinelli_raiola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilieraFrancolettiBelardinelliRaiolaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilieraFrancolettiBelardinelliRaiolaApplication.class, args);
+    }
+
+}
---
+++ Filiera_Francoletti_Belardinelli_Raiola/src/test/java/com/example/filiera_francoletti_belardinelli_raiola/FilieraFrancolettiBelardinelliRaiolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filiera_francoletti_belardinelli_raiola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilieraFrancolettiBelardinelliRaiolaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
