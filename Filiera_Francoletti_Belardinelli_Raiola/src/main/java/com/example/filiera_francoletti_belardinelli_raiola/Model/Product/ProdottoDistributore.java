433ae9a1faec4a74964bc0ee0aaae89575fb4d95
Frenco2003 <marco.francoletti@studenti.unicam.it>
2025-02-21 16:45:27 +0100

    Classi Product

--- Filiera_Francoletti_Belardinelli_Raiola/src/main/java/com/example/filiera_francoletti_belardinelli_raiola/Model/Product/ProdottoDistributore.java
+++ Filiera_Francoletti_Belardinelli_Raiola/src/main/java/com/example/filiera_francoletti_belardinelli_raiola/Model/Product/ProdottoDistributore.java
@@ -1,4 +1,22 @@
 package com.example.filiera_francoletti_belardinelli_raiola.Model.Product;
 
-public class ProdottoDistributore {
+import com.example.filiera_francoletti_belardinelli_raiola.Model.Indirizzo;
+import com.example.filiera_francoletti_belardinelli_raiola.Model.Sellers.Venditore;
+
+import java.util.Date;
+import java.util.List;
+
+public class ProdottoDistributore extends Prodotto{
+
+    private List<Prodotto>listOfProduct;
+    public ProdottoDistributore(String name, double price, String description, Date expiration, Indirizzo processingLocation, Venditore seller,List<Prodotto>listOfProduct) {
+        super(name, price, description, expiration, processingLocation, seller);
+        this.listOfProduct = listOfProduct;
+    }
+    public List<Prodotto> getListOfProduct() {
+        return listOfProduct;
+    }
+    public void setListOfProduct(List<Prodotto> listOfProduct) {
+        this.listOfProduct = listOfProduct;
+    }
 }
