b69bba516712abafc37b10718cd7d007293e6b28
Frenco2003 <marco.francoletti@studenti.unicam.it>
2025-02-23 11:30:52 +0100

    Annotazione Users

--- Filiera_Francoletti_Belardinelli_Raiola/src/main/java/com/example/filiera_francoletti_belardinelli_raiola/Model/Users/UtenteGenerico.java
+++ Filiera_Francoletti_Belardinelli_Raiola/src/main/java/com/example/filiera_francoletti_belardinelli_raiola/Model/Users/UtenteGenerico.java
@@ -1,71 +1,50 @@
 package com.example.filiera_francoletti_belardinelli_raiola.Model.Users;
 
-import com.example.filiera_francoletti_belardinelli_raiola.Controller.HandlerUtente;
-import com.example.filiera_francoletti_belardinelli_raiola.Model.Events.Evento;
-import com.example.filiera_francoletti_belardinelli_raiola.Model.Map.Indirizzo;
-import com.example.filiera_francoletti_belardinelli_raiola.Model.Map.Mappa;
-import com.example.filiera_francoletti_belardinelli_raiola.Model.Product.Prodotto;
+import jakarta.persistence.*;
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="tipo_utente", discriminatorType = DiscriminatorType.STRING)
+public class UtenteGenerico {
 
-import java.util.List;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-public class UtenteGenerico implements Subscriber {
     private String name;
     private String email;
     private String password;
-    private HandlerUtente userHandler;
+
+    public UtenteGenerico() {}
 
     public UtenteGenerico(String name, String email, String password) {
         this.name = name;
         this.email = email;
         this.password = password;
-        this.userHandler=new HandlerUtente();
     }
 
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
-    public void update() {
-        System.out.println("Nuovo evento in piattaforma!");
-    }
-
-    public List<Prodotto> viewProducts() {
-        return userHandler.viewProducts();
-
-    }
-
-    public List<Evento> viewEvents() {
-        return userHandler.viewEvents();
-
-    }
-
-    public Mappa viewMap() {
-        return userHandler.viewMap();
-
-    }
-
-    public Indirizzo traceProduct(int id) {
-        return userHandler.traceProduct(id);
-    }
 }
-
